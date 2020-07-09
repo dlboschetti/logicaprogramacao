fdf9dd43bfb6027f3c5c70d1da29543e2421de10
Diego Boschetti <dlboschetti@gmail.com>
2020-07-09 00:41:35 -0300

    Aula logica programacao

---
+++ LogicaProgramacao/Program.cs
@@ -0,0 +1,62 @@
+﻿    using System;
+
+namespace LogicaProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+
+            if (numero >= 101 )
+            {
+                Console.WriteLine("Condição Verdadeira");
+            }
+            else
+            {
+                Console.WriteLine("Condição Falsa");
+            }
+            ////Exressoes logicas e constantes
+
+          
+            //string nome = "Diego!";
+            //int idade = 37;
+            //string bemvindo = $"Seja bem vindo,{nome}. Sua idade é de: {idade} anos.";
+            ////string frase = bemvindo + nome;
+
+            //Console.WriteLine(bemvindo);
+
+            //const int constInteiro = 20;
+            //int inteiro1 = 2;
+            //int inteiro2 = 23;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+
+
+
+            // aprendendo a declarar variaveis
+            //string hello = "Seja bem vindo a nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.99;
+            //bool booleano = false;
+
+            //Console.WriteLine(hello);
+            //Console.WriteLine(inteiro);
+            //Console.WriteLine(numero);
+            //Console.WriteLine(booleano);
+
+            ////aprendendo sobre arrays
+            //int[] numeros = new int[5];
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 30;
+            //numeros[3] = 40;
+            //numeros[4] = 50;
+
+            //Console.WriteLine(numeros[3]);
+        }
+    }
+}
